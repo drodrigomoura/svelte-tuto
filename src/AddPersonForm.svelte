87f524ba2b86f8eb93feb504c0e4956ab290b4f1
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-06-30 22:07:22 -0300

    form part 2

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -1,21 +1,38 @@
 <script>
-    let name;
-    let beltColour;
-    let age;
+  let name;
+  let beltColour;
+  let age;
 
-    const handleSubmit = () => {
-      console.log(name, beltColour, age);
-      
-    }
+  // let fighting = false;
+  // let sneaking = false;
+  // let running = false;
+  let skills = [];
+  const handleSubmit = () => {
+    console.log(name, beltColour, age, skills);
+  };
 </script>
-
-<form on:submit|preventDefault={handleSubmit}>
-  <input type="text" placeholder="name" bind:value={name}/>
-  <input type="text" placeholder="belt colour" bind:value={beltColour}/>
-  <input type="number" placeholder="age" bind:value={age}>
-  <button>Add Person</button>
-</form>
 
 <style>
 
 </style>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" placeholder="name" bind:value={name} />
+  <!-- <input type="text" placeholder="belt colour" bind:value={beltColour} /> -->
+  <input type="number" placeholder="age" bind:value={age} />
+  <label>Skills:</label>
+  <!-- <input type="checkbox" bind:checked={fighting}/>Fighting<br/>
+  <input type="checkbox" bind:checked={sneaking}/>Sneaking<br/>
+  <input type="checkbox" bind:checked={running}/>Running<br/> -->
+  <input type="checkbox" bind:group={skills} value="fighting"/>Fighting<br/>
+  <input type="checkbox" bind:group={skills} value="sneaking"/>Sneaking<br/>
+  <input type="checkbox" bind:group={skills} value="running"/>Running<br/>
+
+  <select bind:value={beltColour}>
+    <option value="black">black</option>
+    <option value="orange">orange</option>
+    <option value="brown">brown</option>
+    <option value="white">white</option>
+  </select>
+  <button>Add Person</button>
+</form>
